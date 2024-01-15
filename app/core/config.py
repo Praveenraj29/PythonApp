0b269d5499d6a339f7263d5de2da2095795a9275
Larfleeze Gonzo <siddharthprathap@outlook.com>
2024-01-15 10:08:21 +0530

    added init data

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,12 @@
-import secrets
 from typing import List, Union
 
 from pydantic import AnyHttpUrl, validator
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str = "siddharth"
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     # BACKEND_CORS_ORIGINS
--- app/main.py
+++ app/main.py
@@ -4,10 +4,21 @@ from starlette.middleware.cors import CORSMiddleware
 from app.db.engine import Base, engine
 from app.api.api_v1.api import api_router
 from app.core.config import settings
+from app.api.deps import get_db
+from app.db.init_db import init_db
+import contextlib
 
 
 def custom_generate_unique_id(route: APIRoute):
     return f"{route.tags[0]}-{route.name}"
+
+
+get_db_wrapper = contextlib.contextmanager(get_db)
+
+
+def init_data():
+    with get_db_wrapper() as db:
+        init_db(db)
 
 
 app = FastAPI(
@@ -15,12 +26,12 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
-
+origins = [str(origin) for origin in settings.BACKEND_CORS_ORIGINS]
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
@@ -28,6 +39,8 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
+
 @app.on_event("startup")
 async def startup():
     Base.metadata.create_all(bind=engine)
+    init_data()
